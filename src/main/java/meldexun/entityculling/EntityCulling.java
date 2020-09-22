6325a01ebfae8adf56cd96f2f6d6936795f74d01
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-09-22 03:03:08 +0200

    Version 1.0.1

--- src/main/java/meldexun/entityculling/EntityCulling.java
+++ src/main/java/meldexun/entityculling/EntityCulling.java
@@ -13,7 +13,7 @@ public class EntityCulling {
 
 	public static final String MOD_ID = "entity_culling";
 	public static final String NAME = "Entity Culling";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
 	public static final String CLIENT_PROXY_CLASS = "meldexun.entityculling.proxy.ClientProxy";
