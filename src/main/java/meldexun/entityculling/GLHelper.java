d7fa67d72b8fc54479cc96dad538fdd031e37520
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-03-29 16:28:09 +0200

    Updated GLHelper

--- src/main/java/meldexun/entityculling/GLHelper.java
+++ src/main/java/meldexun/entityculling/GLHelper.java
@@ -4,36 +4,45 @@ import org.lwjgl.opengl.ContextCapabilities;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL33;
 import org.lwjgl.opengl.GL43;
+import org.lwjgl.opengl.GL45;
 import org.lwjgl.opengl.GLContext;
 
 public class GLHelper {
 
+	private static boolean openGl45;
 	private static boolean openGl43;
 	private static boolean openGl33;
 
 	public static void init() {
 		ContextCapabilities context = GLContext.getCapabilities();
+		openGl45 = context.OpenGL45;
 		openGl43 = context.OpenGL43;
 		openGl33 = context.OpenGL33;
 	}
 
-	public static void beginQuery(int query) {
-		if (openGl43) {
-			GL15.glBeginQuery(GL43.GL_ANY_SAMPLES_PASSED_CONSERVATIVE, query);
-		} else if (openGl33) {
-			GL15.glBeginQuery(GL33.GL_ANY_SAMPLES_PASSED, query);
+	public static int createQuery() {
+		if (openGl45) {
+			return GL45.glCreateQueries(getQueryTarget());
 		} else {
-			GL15.glBeginQuery(GL15.GL_SAMPLES_PASSED, query);
+			return GL15.glGenQueries();
 		}
 	}
 
+	public static void beginQuery(int query) {
+		GL15.glBeginQuery(getQueryTarget(), query);
+	}
+
 	public static void endQuery() {
+		GL15.glEndQuery(getQueryTarget());
+	}
+
+	private static int getQueryTarget() {
 		if (openGl43) {
-			GL15.glEndQuery(GL43.GL_ANY_SAMPLES_PASSED_CONSERVATIVE);
+			return GL43.GL_ANY_SAMPLES_PASSED_CONSERVATIVE;
 		} else if (openGl33) {
-			GL15.glEndQuery(GL33.GL_ANY_SAMPLES_PASSED);
+			return GL33.GL_ANY_SAMPLES_PASSED;
 		} else {
-			GL15.glEndQuery(GL15.GL_SAMPLES_PASSED);
+			return GL15.GL_SAMPLES_PASSED;
 		}
 	}
 
