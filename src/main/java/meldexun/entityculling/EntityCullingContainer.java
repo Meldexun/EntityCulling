f2b100f035aa861d7bfc48d3bbe61d16434157ee
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-03-29 16:42:43 +0200

    Version 1.12.2-4.1.0

--- src/main/java/meldexun/entityculling/EntityCullingContainer.java
+++ src/main/java/meldexun/entityculling/EntityCullingContainer.java
@@ -41,7 +41,7 @@ public class EntityCullingContainer extends DummyModContainer {
 		super(new ModMetadata());
 		ModMetadata meta = this.getMetadata();
 		meta.name = "Entity Culling";
-		meta.version = "4.0.5";
+		meta.version = "4.1.0";
 		meta.modId = MOD_ID;
 		meta.authorList = Arrays.asList("Meldexun");
 		meta.url = "https://github.com/Meldexun/EntityCulling";
