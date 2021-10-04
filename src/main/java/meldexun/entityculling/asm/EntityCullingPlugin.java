859582fef43ae60239ca843e92459a85bd917623
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-10-04 14:40:59 +0200

    Move EntityCullingPlugin into asm package

--- src/main/java/meldexun/entityculling/asm/EntityCullingPlugin.java
+++ src/main/java/meldexun/entityculling/asm/EntityCullingPlugin.java
@@ -1,21 +1,23 @@
-package meldexun.entityculling.plugin;
+package meldexun.entityculling.asm;
 
 import java.util.Map;
 
+import meldexun.entityculling.EntityCulling;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
-@IFMLLoadingPlugin.TransformerExclusions("meldexun.entityculling.plugin")
+@IFMLLoadingPlugin.TransformerExclusions("meldexun.entityculling.asm")
 public class EntityCullingPlugin implements IFMLLoadingPlugin {
 
 	@Override
 	public String[] getASMTransformerClass() {
-		return new String[] { "meldexun.entityculling.plugin.EntityCullingTransformer" };
+		return new String[] {
+				EntityCullingClassTransformer.class.getName() };
 	}
 
 	@Override
 	public String getModContainerClass() {
-		return "meldexun.entityculling.EntityCullingContainer";
+		return EntityCulling.class.getName();
 	}
 
 	@Override
