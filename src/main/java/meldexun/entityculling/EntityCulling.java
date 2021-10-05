1e2b40e7474528195bd4b9ce461260db9e72559e
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-10-05 18:26:16 +0200

    Update EntityCulling.java

--- src/main/java/meldexun/entityculling/EntityCulling.java
+++ src/main/java/meldexun/entityculling/EntityCulling.java
@@ -2,10 +2,14 @@ package meldexun.entityculling;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import meldexun.entityculling.config.EntityCullingConfig;
+import meldexun.entityculling.util.CullingThread;
+import meldexun.entityculling.util.IBoundingBoxCache;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig.ModConfigEvent;
 import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -31,19 +35,20 @@ public class EntityCulling {
 	public EntityCulling() {
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		ModLoadingContext.get().registerConfig(Type.CLIENT, EntityCullingConfig.CLIENT_SPEC);
-		//ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (mc, lastScreen) -> new ConfigOptionsScreen(lastScreen));
+		// ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (mc, lastScreen) -> new
+		// ConfigOptionsScreen(lastScreen));
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onConfigChanged);
 	}
 
 	private void setup(FMLClientSetupEvent event) {
-		CullingThread.updateBlacklists();
-
-		event.enqueueWork(() -> {
-			GLHelper.init();
-			EntityCullingClient.generateCubeDisplayList();
-			EntityCullingClient.init();
-		});
+		event.enqueueWork(EntityCullingClient::init);
 		MinecraftForge.EVENT_BUS.register(new EntityCullingClient());
+	}
+
+	public void onConfigChanged(ModConfigEvent event) {
+		CullingThread.updateBlacklists();
+		IBoundingBoxCache.updateBlacklist();
 	}
 
 }
