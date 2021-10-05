8e54f9bdb7f0c67dd7f74cb9edcced0ea497beb3
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-10-05 18:26:18 +0200

    Update EntityCullingClient.java

--- src/main/java/meldexun/entityculling/EntityCullingClient.java
+++ src/main/java/meldexun/entityculling/EntityCullingClient.java
@@ -1,138 +1,59 @@
 package meldexun.entityculling;
 
-import static org.lwjgl.opengl.GL15C.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15C.GL_ELEMENT_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15C.GL_STATIC_DRAW;
-import static org.lwjgl.opengl.GL15C.glBindBuffer;
-import static org.lwjgl.opengl.GL15C.glBufferData;
-import static org.lwjgl.opengl.GL15C.glGenBuffers;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.mojang.blaze3d.matrix.MatrixStack;
 
+import meldexun.entityculling.config.EntityCullingConfig;
 import meldexun.entityculling.gui.ShaderOptionsScreen;
-import meldexun.entityculling.plugin.Hook;
+import meldexun.entityculling.util.CullingThread;
+import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.OptionsScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.Entity;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ClassInheritanceMultiMap;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.client.event.GuiScreenEvent;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.TickEvent.Phase;
-import net.minecraftforge.event.entity.EntityLeaveWorldEvent;
-import net.minecraftforge.event.world.ChunkEvent;
-import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EntityCullingClient {
 
-	private static final Logger LOGGER = LogManager.getLogger();
 	public static final CullingThread CULLING_THREAD = new CullingThread();
+	private static final DecimalFormat FORMAT = new DecimalFormat("#.#");
+	public static int renderedTileEntities;
+	public static int culledTileEntities;
 
 	public static void init() {
 		CULLING_THREAD.start();
 	}
 
-	public static int vertexBuffer;
-	public static int indexBuffer;
+	@SubscribeEvent
+	public void onRenderWorldLastEvent(RenderGameOverlayEvent.Post event) {
+		if (event.getType() != ElementType.ALL) {
+			return;
+		}
+		if (!EntityCullingConfig.CLIENT_CONFIG.debug.get()) {
+			return;
+		}
 
-	public static void generateCubeDisplayList() {
-		FloatBuffer vertexByteBuffer = ByteBuffer.allocateDirect(8 * 3 * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		vertexByteBuffer.put(new float[] {
-				0, 0, 0,
-				0, 0, 1,
-				0, 1, 0,
-				0, 1, 1,
-				1, 0, 0,
-				1, 0, 1,
-				1, 1, 0,
-				1, 1, 1
-		});
-		vertexByteBuffer.rewind();
-		ByteBuffer indexByteBuffer = ByteBuffer.allocateDirect(14).order(ByteOrder.nativeOrder());
-		indexByteBuffer.put(new byte[] {
-				0, 4,
-				1, 5,
-				7, 4,
-				6, 0,
-				2, 1,
-				3, 7,
-				2, 6
-		});
-		indexByteBuffer.rewind();
+		Minecraft mc = Minecraft.getInstance();
+		MainWindow window = mc.getWindow();
 
-		vertexBuffer = glGenBuffers();
-		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-		glBufferData(GL_ARRAY_BUFFER, vertexByteBuffer, GL_STATIC_DRAW);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		String s1 = "Time: " + FORMAT.format(Arrays.stream(CULLING_THREAD.time).average().getAsDouble() / 1_000_000.0D) + "ms";
+		String s2 = "E: " + mc.levelRenderer.renderedEntities + "/" + mc.levelRenderer.culledEntities + "/" + mc.level.getEntityCount();
+		String s3 = "TE: " + renderedTileEntities + "/" + culledTileEntities + "/" + mc.level.blockEntityList.size();
 
-		indexBuffer = glGenBuffers();
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexByteBuffer, GL_STATIC_DRAW);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		this.drawOnLeft(event.getMatrixStack(), s1, window.getGuiScaledWidth(), 160);
+		this.drawOnLeft(event.getMatrixStack(), s2, window.getGuiScaledWidth(), 170);
+		this.drawOnLeft(event.getMatrixStack(), s3, window.getGuiScaledWidth(), 180);
 	}
 
-	@SuppressWarnings("resource")
-	@SubscribeEvent
-	public void onWorldTickEvent(TickEvent.ClientTickEvent event) {
-		if (event.phase == Phase.END) {
-			if (EntityCullingConfig.CLIENT_CONFIG.debug.get() && Minecraft.getInstance().level != null && Minecraft.getInstance().level.getGameTime() % 40 == 0) {
-				int i1 = (int) Arrays.stream(CULLING_THREAD.time).sum() / 1_000 / 10;
-				int i2 = Hook.entitiesRendered;
-				int i3 = Hook.entitiesOcclusionCulled;
-				int i4 = Hook.tileEntitiesRendered;
-				int i5 = Hook.tileEntitiesOcclusionCulled;
-				LOGGER.info("Culling Thread: {}µs, Entities Rendered: {}, Entities Culled: {}, TileEntities Rendered: {}, TileEntities Culled: {}", i1, i2, i3, i4, i5);
-			}
-			if (Minecraft.getInstance().level != null && Minecraft.getInstance().level.getGameTime() % 20 == 0) {
-				ICullable.deleteInvalidTileEntityQueries(Minecraft.getInstance().level);
-			}
-		}
-	}
-
-	@SubscribeEvent
-	public void onEntityLeaveWorldEvent(EntityLeaveWorldEvent event) {
-		if (event.getWorld().isClientSide()) {
-			((ICullable) event.getEntity()).deleteQuery();
-		}
-	}
-
-	@SubscribeEvent
-	public void onWorldUnloadEvent(WorldEvent.Unload event) {
-		if (event.getWorld().isClientSide()) {
-			for (Entity e : ((ClientWorld) event.getWorld()).entitiesForRendering()) {
-				((ICullable) e).deleteQuery();
-			}
-			for (TileEntity te : ((ClientWorld) event.getWorld()).blockEntityList) {
-				((ICullable) te).deleteQuery();
-			}
-			ICullable.deleteTileEntityQueries();
-		}
-	}
-
-	@SubscribeEvent
-	public void onChunkUnloadEvent(ChunkEvent.Unload event) {
-		if (event.getWorld().isClientSide()) {
-			for (ClassInheritanceMultiMap<Entity> entityMap : ((Chunk) event.getChunk()).getEntitySections()) {
-				for (Entity e : entityMap) {
-					((ICullable) e).deleteQuery();
-				}
-			}
-			for (TileEntity te : ((Chunk) event.getChunk()).getBlockEntities().values()) {
-				((ICullable) te).deleteQuery();
-			}
-		}
+	private void drawOnLeft(MatrixStack matrixStack, String string, int x, int y) {
+		Minecraft mc = Minecraft.getInstance();
+		mc.font.draw(matrixStack, string, x - mc.font.width(string), y, 0xFFFFFFFF);
 	}
 
 	@SubscribeEvent
@@ -145,9 +66,10 @@ public class EntityCullingClient {
 			return;
 		}
 		Minecraft mc = Minecraft.getInstance();
-		event.addWidget(new Button(screen.width / 2 - 155, screen.height / 6 + 120 - 6 + 24, 150, 20, new TranslationTextComponent("options.entity_culling.shadows.button"), button -> {
-			mc.setScreen(new ShaderOptionsScreen(mc.screen));
-		}));
+		event.addWidget(new Button(screen.width / 2 - 155, screen.height / 6 + 120 - 6 + 24, 150, 20,
+				new TranslationTextComponent("options.entity_culling.shadows.button"), button -> {
+					mc.setScreen(new ShaderOptionsScreen(mc.screen));
+				}));
 	}
 
 }
