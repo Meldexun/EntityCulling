87579be7fc61c717f25b39c41210c6a51443ecb8
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-10-16 23:37:08 +0200

    Update EntityCulling.java

--- src/main/java/meldexun/entityculling/EntityCulling.java
+++ src/main/java/meldexun/entityculling/EntityCulling.java
@@ -1,19 +1,17 @@
 package meldexun.entityculling;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import meldexun.entityculling.config.EntityCullingConfig;
 import meldexun.entityculling.util.CullingThread;
 import meldexun.entityculling.util.IBoundingBoxCache;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.ModConfigEvent;
 import net.minecraftforge.fml.config.ModConfig.Type;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod(EntityCulling.MOD_ID)
 public class EntityCulling {
@@ -33,7 +31,7 @@ public class EntityCulling {
 	}
 
 	public EntityCulling() {
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (s, b) -> true));
 		ModLoadingContext.get().registerConfig(Type.CLIENT, EntityCullingConfig.CLIENT_SPEC);
 		// ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (mc, lastScreen) -> new
 		// ConfigOptionsScreen(lastScreen));
