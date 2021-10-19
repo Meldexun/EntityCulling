c08a5cd51be3b7628ff3e1bb95774cb3a9622b53
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-10-19 14:07:54 +0200

    Update EntityTransformer.js

--- src/main/resources/asm/entity_culling/EntityTransformer.js
+++ src/main/resources/asm/entity_culling/EntityTransformer.js
@@ -10,10 +10,10 @@ function initializeCoreMod() {
 		"Entity Transformer": {
 			"target": {
 				"type": "CLASS",
-				"name": "net.minecraft.entity.Entity"
+				"name": "net.minecraft.world.entity.Entity"
 			},
 			"transformer": function(classNode) {
-				ASMAPI.log("INFO", "Transforming class: net.minecraft.entity.Entity");
+				ASMAPI.log("INFO", "Transforming class: net.minecraft.world.entity.Entity");
 				
 				classNode.fields.add(new FieldNode(Opcodes.ACC_PRIVATE, "isCulled", "Z", null, false));
 				classNode.fields.add(new FieldNode(Opcodes.ACC_PRIVATE, "isShadowCulled", "Z", null, false));
@@ -23,7 +23,7 @@ function initializeCoreMod() {
 				var methodIsCulled = new MethodNode(Opcodes.ACC_PUBLIC, "isCulled", "()Z", null, null);
 				methodIsCulled.instructions.clear();
 				methodIsCulled.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
-				methodIsCulled.instructions.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/entity/Entity", "isCulled", "Z"));
+				methodIsCulled.instructions.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/world/entity/Entity", "isCulled", "Z"));
 				methodIsCulled.instructions.add(new InsnNode(Opcodes.IRETURN));
 				classNode.methods.add(methodIsCulled);
 				
@@ -31,14 +31,14 @@ function initializeCoreMod() {
 				methodSetCulled.instructions.clear();
 				methodSetCulled.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
 				methodSetCulled.instructions.add(new VarInsnNode(Opcodes.ILOAD, 1));
-				methodSetCulled.instructions.add(new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/entity/Entity", "isCulled", "Z"));
+				methodSetCulled.instructions.add(new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/world/entity/Entity", "isCulled", "Z"));
 				methodSetCulled.instructions.add(new InsnNode(Opcodes.RETURN));
 				classNode.methods.add(methodSetCulled);
 				
 				var methodIsShadowCulled = new MethodNode(Opcodes.ACC_PUBLIC, "isShadowCulled", "()Z", null, null);
 				methodIsShadowCulled.instructions.clear();
 				methodIsShadowCulled.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
-				methodIsShadowCulled.instructions.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/entity/Entity", "isShadowCulled", "Z"));
+				methodIsShadowCulled.instructions.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/world/entity/Entity", "isShadowCulled", "Z"));
 				methodIsShadowCulled.instructions.add(new InsnNode(Opcodes.IRETURN));
 				classNode.methods.add(methodIsShadowCulled);
 				
@@ -46,7 +46,7 @@ function initializeCoreMod() {
 				methodSetShadowCulled.instructions.clear();
 				methodSetShadowCulled.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
 				methodSetShadowCulled.instructions.add(new VarInsnNode(Opcodes.ILOAD, 1));
-				methodSetShadowCulled.instructions.add(new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/entity/Entity", "isShadowCulled", "Z"));
+				methodSetShadowCulled.instructions.add(new FieldInsnNode(Opcodes.PUTFIELD, "net/minecraft/world/entity/Entity", "isShadowCulled", "Z"));
 				methodSetShadowCulled.instructions.add(new InsnNode(Opcodes.RETURN));
 				classNode.methods.add(methodSetShadowCulled);
 				
