af715c3e7c3281492b65e829aae05dc54faa00e0
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-02-14 18:16:05 +0100

    Update config min value

--- src/main/java/meldexun/entityculling/config/EntityCullingConfig.java
+++ src/main/java/meldexun/entityculling/config/EntityCullingConfig.java
@@ -32,7 +32,7 @@ public class EntityCullingConfig {
 	@Config.Comment("Most tile entities have static bounding boxes and thus they can be cached. Tile entities whose bounding boxes are likely to change every frame or so should be added to the blacklist. Tile entities whose bounding only change every once in a while should be covered by cache updates (update speed adjustable through tileEntityCachedBoundingBoxUpdateInterval)")
 	public static boolean tileEntityCachedBoundingBoxEnabled = true;
 	@Config.Comment("Every frame there is a 1 in x chance to update the cached bounding box. Higher = better performance, Lower = tile entities with dynamic bounding boxes get updated faster.")
-	@Config.RangeInt(min = 2, max = 1_000_000)
+	@Config.RangeInt(min = 1, max = 1_000_000)
 	public static int tileEntityCachedBoundingBoxUpdateInterval = 100;
 	@Config.RequiresWorldRestart
 	@Config.Comment("Tile entities whose bounding boxes won't be cached (Accepts 'modid' or 'modid:tileentity').")
