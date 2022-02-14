2da436d3c7778bd0e24da5594626d2bd50645bdc
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-02-14 12:41:17 +0100

    Clean up IntUtil

--- src/main/java/meldexun/entityculling/util/IntUtil.java
+++ src/main/java/meldexun/entityculling/util/IntUtil.java
@@ -10,8 +10,8 @@ public class IntUtil {
 	}
 
 	public static boolean anyMatch(int x1, int x2, int y1, int y2, BiIntPredicate predicate) {
-		for (int x = x1; x < x2; x++) {
-			for (int y = y1; y < y2; y++) {
+		for (int x = x1; x <= x2; x++) {
+			for (int y = y1; y <= y2; y++) {
 				if (predicate.test(x, y)) {
 					return true;
 				}
@@ -28,8 +28,8 @@ public class IntUtil {
 	}
 
 	public static void forEach(int x1, int x2, int y1, int y2, BiIntConsumer consumer) {
-		for (int x = x1; x < x2; x++) {
-			for (int y = y1; y < y2; y++) {
+		for (int x = x1; x <= x2; x++) {
+			for (int y = y1; y <= y2; y++) {
 				consumer.accept(x, y);
 			}
 		}
