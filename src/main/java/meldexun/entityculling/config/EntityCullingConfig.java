f8600c751ddd673ce37d1b176db499c53b5a07d3
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-06-28 23:26:53 +0200

    Removed commented config options

--- src/main/java/meldexun/entityculling/config/EntityCullingConfig.java
+++ src/main/java/meldexun/entityculling/config/EntityCullingConfig.java
@@ -36,16 +36,6 @@ public class EntityCullingConfig {
 	//@Config.Comment("If enabled tile entity bounding boxes are increased slightly to avoid issues when other mods don't correctly set their bounding boxes (requires opengl based culling). If you still have culling or flickering issues you can use the 'debugRenderBoxes', 'entityBoundingBoxGrowthList' and 'tileEntityBoundingBoxGrowthList' config options to try to fix the bounding box of that entity or tile entity.")
 	//public static boolean tileEntityAABBGrowth = true;
 
-	//@Config.Comment("Most tile entities have static bounding boxes and thus they can be cached. Tile entities whose bounding boxes are likely to change every frame or so should be added to the blacklist. Tile entities whose bounding only change every once in a while should be covered by cache updates (update speed adjustable through tileEntityCachedBoundingBoxUpdateInterval)")
-	//public static boolean tileEntityCachedBoundingBoxEnabled = true;
-	//@Config.Comment("Every frame there is a 1 in x chance to update the cached bounding box. Higher = better performance, Lower = tile entities with dynamic bounding boxes get updated faster.")
-	//@Config.RangeInt(min = 1, max = 1_000_000)
-	//public static int tileEntityCachedBoundingBoxUpdateInterval = 100;
-	//@Config.Comment("Tile entities whose bounding boxes won't be cached (Accepts 'modid' or 'modid:tileentity').")
-	//public static String[] tileEntityCachedBoundingBoxBlacklist = new String[0];
-	//@Config.Ignore
-	//public static ResourceLocationMap<TileEntity, Boolean> tileEntityCachedBoundingBoxBlacklistImpl = new ResourceLocationMap<>(TileEntity.REGISTRY::getNameForObject, false, s -> true);
-
 	public static EntityOptions entity = new EntityOptions();
 	public static TileEntityOptions tileEntity = new TileEntityOptions();
 	public static OptifineShaderOptions optifineShaderOptions = new OptifineShaderOptions();
@@ -55,9 +45,6 @@ public class EntityCullingConfig {
 	}
 
 	public static void onConfigChanged() {
-		//tileEntityCachedBoundingBoxBlacklistImpl.load(tileEntityCachedBoundingBoxBlacklist);
-		//entity.entityBoundingBoxGrowthListImpl.load(entity.entityBoundingBoxGrowthList);
-		//tileEntity.tileEntityBoundingBoxGrowthListImpl.load(tileEntity.tileEntityBoundingBoxGrowthList);
 		entity.skipHiddenEntityRenderingBlacklistImpl.load(entity.skipHiddenEntityRenderingBlacklist);
 		tileEntity.skipHiddenTileEntityRenderingBlacklistImpl.load(tileEntity.skipHiddenTileEntityRenderingBlacklist);
 	}
@@ -77,15 +64,6 @@ public class EntityCullingConfig {
 		public String[] skipHiddenEntityRenderingBlacklist = new String[0];
 		@Config.Ignore
 		public ResourceLocationMap<Entity, Boolean> skipHiddenEntityRenderingBlacklistImpl = new ResourceLocationMap<>(EntityList::getKey, false, s -> true);
-		//@Config.Comment("Allows you to increase the render bounding boxes of entities (or all entities of a mod). Width increases the size on the X and Z axis. Top increases the size in the positive Y direction. Bottom increases the size in the negative Y direction. (Accepts 'modid=width,top,bottom' or 'modid:entity=width,top,bottom').")
-		//public String[] entityBoundingBoxGrowthList = new String[0];
-		//@Config.Ignore
-		//public ResourceLocationMap<Entity, Vec3d> entityBoundingBoxGrowthListImpl = new ResourceLocationMap<>(EntityList::getKey, null, s -> {
-		//	double x = s.length >= 1 ? Double.parseDouble(s[0]) : 0.0D;
-		//	double y = s.length >= 2 ? Double.parseDouble(s[1]) : 0.0D;
-		//	double z = s.length >= 3 ? Double.parseDouble(s[2]) : 0.0D;
-		//	return x != 0.0D || y != 0.0D || z != 0.0D ? new Vec3d(x, y, z) : null;
-		//});
 
 	}
 
@@ -100,15 +78,6 @@ public class EntityCullingConfig {
 		public String[] skipHiddenTileEntityRenderingBlacklist = new String[0];
 		@Config.Ignore
 		public ResourceLocationMap<TileEntity, Boolean> skipHiddenTileEntityRenderingBlacklistImpl = new ResourceLocationMap<>(TileEntity.REGISTRY::getNameForObject, false, s -> true);
-		//@Config.Comment("Allows you to increase the render bounding boxes of tile entities (or all entities of a mod). Width increases the size on the X and Z axis. Top increases the size in the positive Y direction. Bottom increases the size in the negative Y direction. (Accepts 'modid=width,top,bottom' or 'modid:tileentity=width,top,bottom').")
-		//public String[] tileEntityBoundingBoxGrowthList = new String[0];
-		//@Config.Ignore
-		//public ResourceLocationMap<TileEntity, Vec3d> tileEntityBoundingBoxGrowthListImpl = new ResourceLocationMap<>(TileEntity.REGISTRY::getNameForObject, null, s -> {
-		//	double x = s.length >= 1 ? Double.parseDouble(s[0]) : 0.0D;
-		//	double y = s.length >= 2 ? Double.parseDouble(s[1]) : 0.0D;
-		//	double z = s.length >= 3 ? Double.parseDouble(s[2]) : 0.0D;
-		//	return x != 0.0D || y != 0.0D || z != 0.0D ? new Vec3d(x, y, z) : null;
-		//});
 
 	}
 
@@ -118,26 +87,11 @@ public class EntityCullingConfig {
 		public boolean entityShadowsCullingLessAggressiveMode = true;
 		@Config.RangeDouble(min = 0.0D, max = 1024.0D)
 		public double entityShadowsCullingLessAggressiveModeDiff = 4.0D;
-		//public boolean entityShadowsEnabled = true;
-		//public boolean entityShadowsDistanceLimited = true;
-		//@Config.RangeDouble(min = 0.0D, max = 1024.0D)
-		//public double entityShadowsMaxDistance = 4.0D;
-
-		//public boolean terrainShadowsEnabled = true;
-		//public boolean terrainShadowsDistanceLimited = true;
-		//@Config.RangeDouble(min = 0.0D, max = 1024.0D)
-		//public double terrainShadowsMaxHorizontalDistance = 8.0D;
-		//@Config.RangeDouble(min = 0.0D, max = 1024.0D)
-		//public double terrainShadowsMaxVerticalDistance = 4.0D;
 
 		public boolean tileEntityShadowsCulling = true;
 		public boolean tileEntityShadowsCullingLessAggressiveMode = true;
 		@Config.RangeDouble(min = 0.0D, max = 1024.0D)
 		public double tileEntityShadowsCullingLessAggressiveModeDiff = 4.0D;
-		//public boolean tileEntityShadowsEnabled = true;
-		//public boolean tileEntityShadowsDistanceLimited = true;
-		//@Config.RangeDouble(min = 0.0D, max = 1024.0D)
-		//public double tileEntityShadowsMaxDistance = 4.0D;
 
 	}
 
