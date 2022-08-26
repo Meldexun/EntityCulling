7870ee02c91a4b6fa51e28c957c9b537f9de827d
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-08-26 13:40:16 +0200

    Show culled (tile-) entity count in f3 screen

---
+++ src/main/java/meldexun/entityculling/mixin/MixinGuiOverlayDebug.java
@@ -0,0 +1,44 @@
+package meldexun.entityculling.mixin;
+
+import java.util.List;
+import java.util.function.IntConsumer;
+import java.util.function.Predicate;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import meldexun.renderlib.renderer.EntityRenderManager;
+import meldexun.renderlib.renderer.TileEntityRenderManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiOverlayDebug;
+
+@Mixin(GuiOverlayDebug.class)
+public class MixinGuiOverlayDebug {
+
+	@Shadow
+	private Minecraft mc;
+
+	@Inject(method = "call", at = @At("RETURN"))
+	public void call(CallbackInfoReturnable<List<String>> info) {
+		List<String> list = info.getReturnValue();
+		search(list, s -> s.startsWith("Entities:"), i -> {
+			list.set(i, list.get(i) + ", Culled: " + EntityRenderManager.occludedEntities());
+		});
+		search(list, s -> s.startsWith("Tile Entities:"), i -> {
+			list.set(i, list.get(i) + ", Culled: " + TileEntityRenderManager.occludedTileEntities());
+		});
+	}
+
+	private static void search(List<String> list, Predicate<String> predicate, IntConsumer action) {
+		for (int i = 0; i < list.size(); i++) {
+			if (predicate.test(list.get(i))) {
+				action.accept(i);
+				return;
+			}
+		}
+	}
+
+}
