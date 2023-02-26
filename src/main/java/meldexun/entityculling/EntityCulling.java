1147b98ef3f9d15d53a5bc48469a35b819e2a758
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-02-26 03:11:40 +0100

    Update RenderLib to 1.2.5

--- src/main/java/meldexun/entityculling/EntityCulling.java
+++ src/main/java/meldexun/entityculling/EntityCulling.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-@Mod(modid = EntityCulling.MOD_ID, dependencies = "required-after:renderlib@[1.2.4,)")
+@Mod(modid = EntityCulling.MOD_ID, dependencies = "required-after:renderlib@[1.2.5,)")
 public class EntityCulling {
 
 	public static final String MOD_ID = "entityculling";
