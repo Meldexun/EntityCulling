64bd5bb9e7052ee96eab6a2b964773abf2384107
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-06-19 23:33:29 +0200

    Remove debugRenderBoxes config option
    Moved to RenderLib

--- src/main/java/meldexun/entityculling/mixin/MixinRenderGlobal.java
+++ src/main/java/meldexun/entityculling/mixin/MixinRenderGlobal.java
@@ -6,8 +6,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import meldexun.entityculling.EntityCulling;
-import meldexun.entityculling.config.EntityCullingConfig;
-import meldexun.entityculling.util.BoundingBoxHelper;
 import meldexun.entityculling.util.culling.CullingInstance;
 import meldexun.renderlib.integration.Optifine;
 import meldexun.renderlib.util.RenderUtil;
@@ -30,16 +28,6 @@ public class MixinRenderGlobal {
 				&& (!Optifine.isOptifineDetected() || !Optifine.isShadowPass())) {
 			CullingInstance cullingInstance = CullingInstance.getInstance();
 			cullingInstance.updateResults(RenderUtil.getProjectionModelViewMatrix());
-		}
-	}
-
-	/** {@link RenderGlobal#renderEntities(Entity, ICamera, float)} */
-	@Inject(method = "renderEntities", at = @At("RETURN"))
-	public void postEntities(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo info) {
-		if (EntityCullingConfig.debugRenderBoxes
-				&& MinecraftForgeClient.getRenderPass() == 1
-				&& (!Optifine.isOptifineDetected() || !Optifine.isShadowPass())) {
-			BoundingBoxHelper.getInstance().drawRenderBoxes(partialTicks);
 		}
 	}
 
